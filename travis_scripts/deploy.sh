52e8b11d9f58cda19730fbf67122649c8185cf5e
Ludo <LudoBermejo@gmail.com>
2016-12-22 19:39:00 +0100

    .gitignore

--- travis_scripts/deploy.sh
+++ travis_scripts/deploy.sh
@@ -5,16 +5,18 @@ ssh-keyscan ludobermejo.es >> ~/.ssh/known_hosts
 if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
     cd _site
-    ls -la
     git init
 
     git remote add deploy "deploy@ludobermejo.es:/var/www/remotes/yeelight-server"
     git config user.name "Travis CI"
     git config user.email "ludobermejo+travisCI@gmail.com"
-    rm -rf .gitignore
+    rm .gitignore
+    rm .travis.yml
+    rm -rf travis_scripts
     git add .
     git commit -m "Deploy"
     git push --force deploy master
+    ls -la
 else
     echo "Not deploying, since this branch isn't master."
 fi
--- travis_scripts/install.sh
+++ travis_scripts/install.sh
@@ -3,7 +3,6 @@ set -x # Show the output of the following commands (useful for debugging)
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_97b3e9460dc1_key -iv $encrypted_97b3e9460dc1_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
-rm .gitignore
 chmod 600 deploy-key
 mv deploy-key ~/.ssh/id_rsa
 eval `ssh-agent -s` #start shh agent

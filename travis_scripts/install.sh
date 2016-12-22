e018efaff8a26d970348a04749409fcb6772da9e
Ludo <LudoBermejo@gmail.com>
2016-12-22 19:06:00 +0100

    Remove gitignore

--- travis_scripts/install.sh
+++ travis_scripts/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf .gitignore
 set -x # Show the output of the following commands (useful for debugging)
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_97b3e9460dc1_key -iv $encrypted_97b3e9460dc1_iv -in deploy-key.enc -out deploy-key -d
@@ -9,4 +10,3 @@ eval `ssh-agent -s` #start shh agent
 ssh-add ~/.ssh/id_rsa
 yarn
 yarn install
-ls -la

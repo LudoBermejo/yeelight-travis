b18255c3331a6dab972d2bd31a6b9c9fd6308ff9
Ludo <LudoBermejo@gmail.com>
2016-12-22 20:11:37 +0100

    Other

--- travis_scripts/install.sh
+++ travis_scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-rm -rf .gitignore
 set -x # Show the output of the following commands (useful for debugging)
 # Import the SSH deployment key
 openssl aes-256-cbc -K $encrypted_97b3e9460dc1_key -iv $encrypted_97b3e9460dc1_iv -in deploy-key.enc -out deploy-key -d
 rm deploy-key.enc # Don't need it anymore
 chmod 600 deploy-key
+echo "" > .gitignore
 mv deploy-key ~/.ssh/id_rsa
 eval `ssh-agent -s` #start shh agent
 ssh-add ~/.ssh/id_rsa

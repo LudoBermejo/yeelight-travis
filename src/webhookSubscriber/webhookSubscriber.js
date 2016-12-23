1eb1c13c90ca4aa5f3667b87c1f79cd19fc1a8db
Ludo <LudoBermejo@gmail.com>
2016-12-23 17:03:03 +0100

    Colors

--- src/webhookSubscriber/webhookSubscriber.js
+++ src/webhookSubscriber/webhookSubscriber.js
@@ -4,9 +4,26 @@ const yeelightController = require('./../yeelight/yeelightController');
 const client = new faye.Client('http://ludobermejo.es:9100/faye');
 
 function subscribe() {
-  const subscription = client.subscribe('/webhookDispatched', (message) => {
-    console.log(message);
-    yeelightController.toggle();
+  client.subscribe('/webhookDispatched', (message) => {
+    const data = JSON.parse(message.text);
+    switch (data.state) {
+      case 'started':
+        yeelightController.turnOn();
+        yeelightController.setRGB('#F3F781');
+        break;
+      case 'passed':
+        yeelightController.turnOn();
+        yeelightController.setRGB('#00FF00');
+        setTimeout(() => yeelightController.turnOff(), 5000);
+        break;
+      case 'broken':
+      case 'error':
+        yeelightController.turnOn();
+        yeelightController.setRGB('#FF0000');
+        break;
+      default:
+        break;
+    }
   });
 }
 function init() {

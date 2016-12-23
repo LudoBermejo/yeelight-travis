967012dc912e59ba8a125a79fae900d017bd5519
Ludo <LudoBermejo@gmail.com>
2016-12-23 15:22:40 +0100

    Fix problems with connection

--- api.js
+++ api.js
@@ -1,8 +1,3 @@
 const graphQL = require('./src/graphQL/graphQL');
-const winston = require('winston');
 
-graphQL.connect()
-.catch(e => {
-  winston.log('error', e);
-    process.exit();
-});
+graphQL.connect();

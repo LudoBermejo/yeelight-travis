73697591638c57e1f4c9ed14105baeff7b4c1936
Ludo Bermejo <ludobermejo@gmail.com>
2016-12-23 17:19:14 +0100

    New color on start

--- src/yeelight/yeelightController.js
+++ src/yeelight/yeelightController.js
@@ -23,7 +23,7 @@ function turnOn(lightBulb) {
 function start(lightBulb) {
   const mylightBulb = lightBulb || arrayLightBulb[0];
   mylightBulb.turnOn();
-  mylightBulb.setRGB('#F3F781');
+  mylightBulb.setRGB('#f76a11');
 }
 
 function complete(lightBulb) {
